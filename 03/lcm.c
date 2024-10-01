ea9ee86401e87c951eccbc743f4cf9fff5633480
Pataterecta <140719296+Pataterecta@users.noreply.github.com>
2024-10-01 07:45:48 +0200

    Add files via upload

--- 03/lcm.c
+++ 03/lcm.c
@@ -1,36 +1,36 @@
-Assignment name  : lcm
-Expected files   : lcm.c
-Allowed functions:
---------------------------------------------------------------------------------
+// Assignment name  : lcm
+// Expected files   : lcm.c
+// Allowed functions:
+// --------------------------------------------------------------------------------
 
-Write a function who takes two unsigned int as parameters and returns the
-computed LCM of those parameters.
+// Write a function who takes two unsigned int as parameters and returns the
+// computed LCM of those parameters.
 
-LCM (Lowest Common Multiple) of two non-zero integers is the smallest postive
-integer divisible by the both integers.
+// LCM (Lowest Common Multiple) of two non-zero integers is the smallest postive
+// integer divisible by the both integers.
 
-A LCM can be calculated in two ways:
+// A LCM can be calculated in two ways:
 
-- You can calculate every multiples of each integers until you have a common
-multiple other than 0
+// - You can calculate every multiples of each integers until you have a common
+// multiple other than 0
 
-- You can use the HCF (Highest Common Factor) of these two integers and
-calculate as follows:
+// - You can use the HCF (Highest Common Factor) of these two integers and
+// calculate as follows:
 
-	LCM(x, y) = | x * y | / HCF(x, y)
+// 	LCM(x, y) = | x * y | / HCF(x, y)
 
-  | x * y | means "Absolute value of the product of x by y"
+//   | x * y | means "Absolute value of the product of x by y"
 
-If at least one integer is null, LCM is equal to 0.
+// If at least one integer is null, LCM is equal to 0.
 
-Your function must be prototyped as follows:
+// Your function must be prototyped as follows:
 
-  unsigned int    lcm(unsigned int a, unsigned int b);
+//   unsigned int    lcm(unsigned int a, unsigned int b);
 
-두 개의 양의 정수에 대해 최소 공배수를 구하는 함수를 작성하라는 내용 
-주어진 두 수가 0이 아닌 경우 lcm
-둘 중 하나라도 0이면 lcm 은 0
-lcm(4, 6) = 12
+// 두 개의 양의 정수에 대해 최소 공배수를 구하는 함수를 작성하라는 내용 
+// 주어진 두 수가 0이 아닌 경우 lcm
+// 둘 중 하나라도 0이면 lcm 은 0
+// lcm(4, 6) = 12
 
 unsigned int lcm(unsigned int a, unsigned int b)
 {
